6d2084061e16deb3b10e6cad2e1bad944a0c4d59
thatojadezweni <thato.malope2000@gmail.com>
2025-01-29 12:41:05 +0200

    Added identity module.

---
+++ main/Modules/Identity/Flare.Modules.Identity.Application/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Flare.Modules.Identity.Application;
+
+public static class DependencyInjection
+{
+	public static IServiceCollection AddIdentityModule(this IServiceCollection services, IConfiguration configuration)
+	{
+		return services;
+	}
+}
